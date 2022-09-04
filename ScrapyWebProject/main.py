6c74fd827b72290e90afebdb2c143f6952c06741
thatgirlprogrammer <mullins.amy@gmail.com>
2022-09-04 16:13:29 -0400

    Add files via upload

--- ScrapyWebProject/main.py
+++ ScrapyWebProject/main.py
@@ -1,9 +1,69 @@
-from scrapy.crawler import CrawlerProcess
-from tutorial.tutorial.spiders.QuotesSpider import *
+import json
+import string
+import nltk as nltk
 
-process = CrawlerProcess({
-    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-})
+f = open('tutorial/ksuemail.json')
+data = json.load(f)
+word_freq = {}
+word_freq2 = {}
+email_freq = {}
+email_page_count = 0
+total_words = 0
 
-process.crawl(EmailtrackSpider)
-process.start()
+
+def update_dict(dict, lst):
+    for value in lst:
+        if value in dict:
+            dict[value] += 1
+        else:
+            dict.update({value: 1})
+
+
+stop_words = nltk.corpus.stopwords.words("english")
+for i in data:
+    if len(i['emails']) > 0:
+        email_page_count += 1
+        update_dict(email_freq, i['emails'])
+
+    wordLst = i['body'].split(" ")
+    total_words += len(wordLst)
+    update_dict(word_freq, wordLst)
+    wordLst = i['body'].translate(str.maketrans('', '', string.punctuation)).split(" ")
+
+    word_tokens = nltk.word_tokenize(i['body'])
+    filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
+    update_dict(word_freq2, filtered_sentence)
+
+email_freq = {k: v for k, v in sorted(email_freq.items(), key=lambda item: item[1], reverse=True)}
+word_freq = {k: v for k, v in sorted(word_freq.items(), key=lambda item: item[1], reverse=True)}
+word_freq2 = {k: v for k, v in sorted(word_freq2.items(), key=lambda item: item[1], reverse=True)}
+
+print("Average page length:", total_words / len(data))
+print(email_freq)
+print("Percentage of pages with emails:", email_page_count / len(data) * 100)
+
+print("Top word freq before removing punctuation and stopwords")
+print("randk\t\tterm\t\t\tfreq")
+print("------\t\t--------\t\t-------")
+count = 1
+for key in word_freq:
+    if count <= 30:
+        print(count, "\t\t", key, "\t\t\t", word_freq[key] / total_words * 100)
+        count += 1
+    else:
+        break
+
+total_words = 0
+for key in word_freq2:
+    total_words += word_freq2[key]
+
+print("Top word freq after removing punctuation and stopwords")
+print("randk\t\tterm\t\t\tfreq")
+print("------\t\t--------\t\t-------")
+count = 1
+for key in word_freq2:
+    if count <= 30:
+        print(count, "\t\t", key, "\t\t\t", word_freq2[key] / total_words * 100)
+        count += 1
+    else:
+        break
--- ScrapyWebProject/tutorial/tutorial/spiders/QuotesSpider.py
+++ ScrapyWebProject/tutorial/tutorial/spiders/QuotesSpider.py
@@ -1,21 +1,12 @@
-# web scraping framework
 from scrapy.linkextractors import LinkExtractor
-
-# for regular expression
 import re
-
-# for selenium request
 from bs4 import BeautifulSoup as BS
 from scrapy.spiders import CrawlSpider, Rule
-#from scrapy_selenium import SeleniumRequest
-
-# for link extraction
-#from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
 import json
 import scrapy
 
 
-def clean_wordlist(wordlist):
+"""def clean_wordlist(wordlist):
 	clean_list = []
 	for word in wordlist:
 		symbols = "!@#$%^&*()_-+={[}]|\;:\"<>?/., "
@@ -25,9 +16,7 @@ def clean_wordlist(wordlist):
 
 		if len(word) > 0:
 			clean_list.append(word)
-	# create_dictionary(clean_list)
-
-
+"""
 
 class EmailtrackSpider(CrawlSpider):
 	# name of spider
@@ -39,7 +28,7 @@ class EmailtrackSpider(CrawlSpider):
 	]
 	custom_settings = {
 		'USER_AGENT': "KSU-CS4422-IRbot/0.1",
-		'CLOSESPIDER_PAGECOUNT': 100,
+		'CLOSESPIDER_PAGECOUNT': 200,
 	}
 	rules = [
 		Rule(
@@ -65,140 +54,6 @@ class EmailtrackSpider(CrawlSpider):
 
 		entry['emails'] = re.findall(self.regex, entry['body'])
 		yield entry
-	#
-	# def get_requests(self):
-	# 	return [SeleniumRequest(url="https://ccse.kennesaw.edu/",
-	# 							wait_time=2, screenshot=True,
-	# 							callback=self.parse,
-	# 							dont_filter=True
-	# 							), SeleniumRequest(
-	# 		url="https://www.kennesaw.edu/",
-	# 		wait_time=2,
-	# 		screenshot=True,
-	# 		callback=self.parse,
-	# 		dont_filter=True
-	# 	), SeleniumRequest(
-	# 		url="https://datascience.kennesaw.edu/",
-	# 		wait_time=2,
-	# 		screenshot=True,
-	# 		callback=self.parse,
-	# 		dont_filter=True
-	# 	)
-	# 			]
-	#
-	# # start_requests sends request to given https://www.geeksforgeeks.org/
-	# # and parse function is called
-	# def start_requests(self):
-	# 	for i in range(len(self.get_requests())):
-	# 		yield self.get_requests()[i]
-	#
-	# def parse(self, response):
-	# 		# this helps to get all links from source code
-	# 		links = LxmlLinkExtractor(allow=()).extract_links(response)
-	#
-	# 		# Finallinks contains links urk
-	# 		Finallinks = [str(link.url) for link in links]
-	#
-	# 		# links list for url that may have email ids
-	# 		links = []
-	#
-	# 		# filtering and storing only needed url in links list
-	# 		# pages that are about us and contact us are the ones that have email ids
-	# 		for link in Finallinks:
-	# 			links.append(link)
-	#
-	# 		# current page url also added because few sites have email ids on there main page
-	# 		links.append(str(response.url))
-	#
-	# 		# parse_link function is called for extracting email ids
-	# 		l = links[0]
-	# 		links.pop(0)
-	#
-	# 		# meta helps to transfer links list from parse to parse_link
-	# 		yield SeleniumRequest(
-	# 			url=l,
-	# 			wait_time=2,
-	# 			screenshot=True,
-	# 			callback=self.parse_link,
-	# 			dont_filter=True,
-	# 			meta={'links': links}
-	# 		)
-	#
-	# def parse_link(self, response):
-	# 	# response.meta['links'] this helps to get links list
-	# 	links = response.meta['links']
-	# 	flag = 0
-	# 	emails = []
-	# 	wordlist = []
-	# 	count_pages = 0
-	# 	entry = dict.fromkeys(['pageid', 'url', 'title', 'body', 'emails'])
-	#
-	# 	# if flag is 1 then no need to get email from
-	# 	# that url/page
-	# 	if flag != 1:
-	# 		html_text = str(response.text)
-	# 		# regular expression used for email id
-	# 		email_list = re.findall('\w+@\w+\.{1}\w+', html_text)
-	#
-	# 		if len(email_list) != 0:
-	# 			count_pages += 1
-	# 			for i in email_list:
-	# 				emails.append(i)
-	# 				if i not in self.email_freq:
-	# 					self.email_freq.update({i: 1})
-	# 				else:
-	# 					self.email_freq[i] += 1
-	#
-	# 	# self.get_word_frequencies(links, wordlist)
-	#
-	# 	if len(links) > 0:
-	# 		l = links[0]
-	# 		links.pop(0)
-	#
-	# 		yield SeleniumRequest(
-	# 			url=l,
-	# 			callback=self.parse_link,
-	# 			dont_filter=True,
-	# 			meta={'links': links}
-	# 		)
-	# 	else:
-	# 		yield SeleniumRequest(
-	# 			url=response.url,
-	# 			callback=self.parsed,
-	# 			dont_filter=True
-	# 		)
-	#
-	# 	print(self.word_freq)
-	# 	with open('KSU100.json', 'w', encoding='utf-8') as f:
-	# 		json.dump(self.email_freq, f, ensure_ascii=False, indent=4)
-	# 	return entry
-
-	# def get_word_frequencies(self, links, wordlist):
-	# 	if len(links) > 0:
-	# 		source_code = requests.get(links[0]).text
-	# 		soup = BeautifulSoup(source_code, 'html.parser')
-
-			# for each_text in soup.findAll('div', {'class': 'entry-content'}):
-			# 	content = each_text.text
-			# 	words = content.lower().split()
-			# 	for word in words:
-			# 		wordlist.append(word)
-			#
-			# 	for word in wordlist:
-			# 		symbols = "!@#$%^&*()_-+={[}]|\;:\"<>?/., "
-			#
-			# 		for i in range(len(symbols)):
-			# 			word = word.replace(symbols[i], '')
-			#
-			# 		clean_list = []
-			# 		if len(word) > 0:
-			# 			clean_list.append(word)
-			#
-			# 		for word in clean_list:
-			# 			if word in self.word_freq:
-			# 				self.word_freq[word] += 1
-			# 			else:
-			# 				self.word_freq[word] = 1
 
 	def parsed(self, response):
 		# emails list of uniqueemail set
@@ -206,8 +61,6 @@ class EmailtrackSpider(CrawlSpider):
 		finalemail = []
 
 		for email in emails:
-			# avoid garbage value by using '.in' and '.com'
-			# and append email ids to finalemail
 			if ('.in' in email or '.com' in email or 'info' in email or 'org' in email):
 				finalemail.append(email)
 
